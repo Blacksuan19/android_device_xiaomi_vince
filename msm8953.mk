3c9955e55debc746c2637c346539833b48907634
mdeejay <mdjrussia@gmail.com>
2018-05-05 14:19:07 +0700

    Switch to SnapdragonCamera2

--- msm8953.mk
+++ msm8953.mk
@@ -131,7 +131,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
-    SnapdragonCamera \
+    SnapdragonCamera2 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
