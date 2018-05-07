1a3d3927ca3021dc64cc6a46842bc90f082bb094
mdeejay <mdjrussia@gmail.com>
2018-05-07 23:23:50 +0700

    msm8953-common: typos

--- prop.mk
+++ prop.mk
@@ -199,10 +199,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 wifi.interface=wlan0
 
 # Trim properties
-ro.vendor.qti.sys.fw.use_trim_settings=true
-ro.vendor.qti.sys.fw.empty_app_percent=50
-ro.vendor.qti.sys.fw.trim_empty_percent=100
-ro.vendor.qti.sys.fw.trim_cache_percent=100
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.vendor.qti.sys.fw.use_trim_settings=true \
+ro.vendor.qti.sys.fw.empty_app_percent=50 \
+ro.vendor.qti.sys.fw.trim_empty_percent=100 \
+ro.vendor.qti.sys.fw.trim_cache_percent=100 \
 ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
 
 # Higher fling velocities to smooth scrolling
