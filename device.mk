929dc1735a3c5ffe98a803830807fdee72d1c937
mdeejay <mdjrussia@gmail.com>
2018-05-12 13:17:51 +0700

    vince: drop dummy libkeymaster1

--- device.mk
+++ device.mk
@@ -239,8 +239,7 @@ PRODUCT_COPY_FILES += \
 # Keymaster HAL
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
-    libkeymaster1
+    android.hardware.keymaster@3.0-service
 
 # Consumerir
 PRODUCT_PACKAGES += \
