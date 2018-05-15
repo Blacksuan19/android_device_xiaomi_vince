6e75cccfba85530295bd261b58f336593b206dd2
mdeejay <mdjrussia@gmail.com>
2018-05-15 16:12:38 +0700

    vince: add XiaomiDoze

--- device.mk
+++ device.mk
@@ -159,6 +159,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.0 \
     vendor.display.config@1.0_vendor
 
+# Doze
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
