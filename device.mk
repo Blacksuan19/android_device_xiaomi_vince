eb53c1629a8edba52f4ad410d2272e0c25f2e539
mdeejay <mdjrussia@gmail.com>
2018-06-03 23:04:06 +0700

    vince: Added prebuilt CameraNextMod by PMS

--- device.mk
+++ device.mk
@@ -134,7 +134,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
-    Snap \
+    CameraNextMod \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
