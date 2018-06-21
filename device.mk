da579df1776ba6e05d8aa24d18bf15e2a5a50735
Madkita <abubakaryagob@gmail.com>
2018-06-22 04:50:14 +0800

    add prebuild Modded Gcam

--- device.mk
+++ device.mk
@@ -130,7 +130,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
-    Snap \
+    MGC \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
