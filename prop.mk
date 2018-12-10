14cb47dfcd08e905241e6198d0c3846f73fbf830
mdeejay <mdjrussia@gmail.com>
2018-12-10 14:41:00 +0100

    vince: update props for P

--- prop.mk
+++ prop.mk
@@ -51,10 +51,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    camera.display.lmax=1280x720 \
-    camera.display.umax=1920x1080 \
-    camera.hal1.packagelist=com.skype.raider,com.google.android.talk,com.whatsapp \
-    camera.lowpower.record.enable=1 \
+    persist.vendor.camera.display.umax=1920x1080 \
+	persist.vendor.camera.display.lmax=1280x720 \
+    vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk,com.whatsapp \
+    vendor.camera.lowpower.record.enable=1 \
     media.camera.ts.monotonic=1 \
     persist.camera.gyro.disable=0 \
     persist.camera.isp.clock.optmz=0 \
