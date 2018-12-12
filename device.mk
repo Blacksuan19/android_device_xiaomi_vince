21e5617e4e8725fb7abc73022cd9c35c5e1a186d
ghpranav <bedrepranav@gmail.com>
2018-12-12 07:51:40 +0100

    vince: Move QTI whitelist to vendor

--- device.mk
+++ device.mk
@@ -377,7 +377,7 @@ PRODUCT_PACKAGES += \
 
 # Whitelisted app
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/qti_whitelist.xml
 
 # Wifi
 PRODUCT_PACKAGES += \
