74a24e625293a8e236d28bae44289518f87a37d5
mdeejay <mdjrussia@gmail.com>
2018-12-13 17:02:41 +0100

    vince: my kernel using input1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,7 +202,7 @@ TARGET_PER_MGR_ENABLED := true
 # Power
 TARGET_HAS_NO_WLAN_STATS := true
 TARGET_USES_INTERACTION_BOOST := true
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc/78b7000.i2c/i2c-3/3-0020/input/input2/wake_gesture"
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc/78b7000.i2c/i2c-3/3-0020/input/input1/wake_gesture"
 
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
