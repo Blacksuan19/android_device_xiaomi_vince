418904860a322ed0cf8f18d7a876dfaf9433f1e0
Flex1911 <dedsa2002@gmail.com>
2018-12-22 19:14:04 +0100

    Kill HIDL ANT+ support
    CAF removed it for 8953 in P. Furthermore, I'm not sure if it ever worked for us.

--- device.mk
+++ device.mk
@@ -78,7 +78,8 @@ PRODUCT_COPY_FILES += \
 # ANT
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library
+    com.dsi.ant.antradio_library \
+    libantradio
 	
 # Additional native libraries
 PRODUCT_COPY_FILES += \
