4e40bfa3f8a86e3b446af2c202f2be05e64bc21f
Demon000 <demonsingur@gmail.com>
2019-01-01 07:29:26 +0100

    vince: Convert lights HAL into a native binderized HAL

--- device.mk
+++ device.mk
@@ -264,9 +264,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8953
+    android.hardware.light@2.0-service.vince
 	
 # Media
 PRODUCT_COPY_FILES += \
