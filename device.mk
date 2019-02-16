919945bdfe61df747b1b0039c835d0ee83da403c
Abubakar Yagoub <Abubakaryagob@gmail.com>
2019-02-16 12:41:50 +0800

    Revert "vince: Enable QCOM Boost framework"
    This reverts commit 166b1e0d557918d70a0c11c400aa3a0ad8fdc5c0.
    
    Signed-off-by: Abubakar Yagoub <Abubakaryagob@gmail.com>

--- device.mk
+++ device.mk
@@ -319,11 +319,6 @@ PRODUCT_COPY_FILES += \
 # QMI
 PRODUCT_PACKAGES += \
     libjson
-	
-#QTI performance
-PRODUCT_BOOT_JARS += \
-    QPerformance \
-    UxPerformance
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
