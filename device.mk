03ab70bc9577ce9f609e956fa7d91a02dbcdd723
Abubakar Yagoub <Abubakaryagob@gmail.com>
2019-03-07 19:42:14 +0800

    Revert "vince: Add AdvancedControl"
    This reverts commit a5ea13c6da72f1e44f0f6d93af0b5bdc34fedc03.
    
    Signed-off-by: Abubakar Yagoub <Abubakaryagob@gmail.com>

--- device.mk
+++ device.mk
@@ -83,10 +83,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Advanced Controls
-PRODUCT_PACKAGES += \
-	AdvancedControls
-
 # ANT
 PRODUCT_PACKAGES += \
     AntHalService \
--- prop.mk
+++ prop.mk
@@ -102,7 +102,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.vendor.display.cabl=2 \
 	sdm.debug.disable_skip_validate=1 \
 	vendor.display.disable_skip_validate=1 \
-	vendor.display.enable_default_color_mode=0 \
+	vendor.display.enable_default_color_mode=1 \
 	vendor.gralloc.enable_fb_ubwc=1
 	
 # DRM
