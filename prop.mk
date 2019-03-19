5ceb8d5a2f34109f366ac65fd5fadc95d13bc1ba
Abubakar Yagoub <Abubakaryagob@gmail.com>
2019-03-19 22:12:55 +0800

    prop: set debug.sf.latch_unsignaled = 1
    Signed-off-by: Abubakar Yagoub <Abubakaryagob@gmail.com>

--- prop.mk
+++ prop.mk
@@ -90,6 +90,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	debug.sf.enable_hwc_vds=1 \
 	debug.sf.hw=0 \
 	debug.sf.recomputecrop=0 \
+    debug.sf.latch_unsignaled=1 \
 	dev.pm.dyn_samplingrate=1 \
 	persist.debug.wfd.enable=1 \
 	persist.demo.hdmirotationlock=false \
