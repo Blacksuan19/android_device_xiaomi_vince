f22e60f94670a608ffe6bae49bc3bfe1d97ec1ed
Abubakar Yagoub <Abubakaryagob@gmail.com>
2019-03-19 21:40:23 +0800

    rootdir: fix derp
    Signed-off-by: Abubakar Yagoub <Abubakaryagob@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -77,6 +77,7 @@ LOCAL_SRC_FILES    := etc/init.qcom.early_boot.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE	   := move_time_data.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
