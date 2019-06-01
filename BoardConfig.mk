b55e4d1a106ad34410dbed367128b57ee70b7dfd
Blacksuan19 <Abubakaryagob@gmail.com>
2019-06-01 10:08:38 +0800

    Board: dont set soong namespaces
    Signed-off-by: Blacksuan19 <Abubakaryagob@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,8 +51,3 @@ BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 TARGET_QCOM_DISPLAY_VARIANT := caf-msm8996
 TARGET_QCOM_AUDIO_VARIANT := caf-msm8996
 TARGET_QCOM_MEDIA_VARIANT := caf-msm8996
- 
-PRODUCT_SOONG_NAMESPACES += \
-       hardware/qcom/display-$(TARGET_QCOM_DISPLAY_VARIANT) \
-       hardware/qcom/audio-$(TARGET_QCOM_AUDIO_VARIANT) \
-       hardware/qcom/media-$(TARGET_QCOM_MEDIA_VARIANT)
