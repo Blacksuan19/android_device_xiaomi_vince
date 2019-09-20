f6fd8e9b8fb27042582da333b3211d78b4aba375
PrateekPunetha <prateekpunetha@gmail.com>
2019-09-20 11:25:00 +0300

    vince: Removed lineage specific services
    kthxbye
    Signed-off-by: PrateekPunetha <prateekpunetha@gmail.com>

--- device.mk
+++ device.mk
@@ -22,8 +22,6 @@ $(call inherit-product, vendor/nitrogen/config/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
-
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
@@ -62,6 +60,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
-# Touch HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.xiaomi_8953
