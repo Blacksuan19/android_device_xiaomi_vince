706240a60e8a4c2b83597e182b387c99e864f1ec
Pranav Vashi <neobuddy89@gmail.com>
2020-01-26 22:52:38 +0530

    vince: Use single overlay dir
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -19,8 +19,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
-
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
