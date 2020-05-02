55bbb88cc93be6f2e4231d72046f0bf6ffa0c1d3
KenHV <kenhv@null.net>
2020-05-02 08:25:53 +0000

    vince: update props
    revert back to old noise cancellation settings, add more apps to aux packagelist and fixup syntax
    
    Signed-off-by: KenHV <kenhv@null.net>
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- prop.mk
+++ prop.mk
@@ -1,15 +1,26 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.audio.fluence.speaker=true \
-    persist.vendor.audio.fluence.voicecall=true \
-    persist.vendor.audio.fluence.voicerec=false \
-    ro.vendor.audio.sdk.fluencetype=fluence
+persist.vendor.audio.fluence.speaker=true \
+persist.vendor.audio.fluence.voicecall=true \
+persist.vendor.audio.fluence.voicerec=false \
+ro.vendor.audio.sdk.fluencetype=fluence
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
-    persist.camera.dual.camera=0 \
-    persist.camera.CDS=off
+vendor.camera.aux.packageblacklist=com.discord \
+persist.camera.HAL3.enabled=1 \
+vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera2,com.google.android.GoogleCamera \
+vendor.camera.aux.packagelist2=com.google.android.GoogleCameraWide,com.dual.GCam,com.Wide.GCam,com.Tele.GCam \
+vendor.camera.hal1.packagelist=com.whatsapp \
+persist.camera.dual.camera=0 \
+persist.camera.eis.enable=1
+
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.sys.usb.config=mtp,adb \
+ro.adb.secure=0 \
+ro.secure=0 \
+ro.debuggable=1
 
 # maintainer
 ro.havoc.maintainer=Blacksuan19
